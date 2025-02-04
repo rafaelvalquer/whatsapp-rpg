4065e692eaf23b12527c2dad27967df3c0486eae
RAFAEL VALQUER MORAES DOS SANTOS <rafael.moraes@bgp.local>
2025-02-04 14:43:49 -0300

    v1.57

--- Backend/db.js
+++ Backend/db.js
@@ -1,5 +1,7 @@
 require("dotenv").config();
 
 module.exports = {
-    uri: `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASSWORD}@cluster0.9nsk1.mongodb.net/rpgGame?retryWrites=true&w=majority`,
+    //uri: `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASSWORD}@cluster0.9nsk1.mongodb.net/rpgGame?retryWrites=true&w=majority`,
+    uri: "mongodb+srv://rafaelvalquer:mudar123.@cluster0.9nsk1.mongodb.net/rpgGame?retryWrites=true&w=majority&appName=Cluster0"
+
 };
