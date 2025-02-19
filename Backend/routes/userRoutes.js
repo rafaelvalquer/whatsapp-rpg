f868d8e098bfeffb97861073b76d6218a84d3ab0
RAFAEL VALQUER MORAES DOS SANTOS <rafael.moraes@bgp.local>
2025-02-19 14:37:13 -0300

    v1.118

--- Backend/routes/userRoutes.js
+++ Backend/routes/userRoutes.js
@@ -1,8 +1,7 @@
 const express = require("express");
-const router = express.Router();
 const userController = require("../controllers/userController");
 
-
+const router = express.Router();
 
 router.post("/check-number", userController.checkUserByPhone);
 router.post("/check-user", userController.checkUserByName);
