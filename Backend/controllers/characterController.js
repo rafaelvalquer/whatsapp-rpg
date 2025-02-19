ca72fd855e9f12a7d7d125c31a6a3a3cd09f9b65
RAFAEL VALQUER MORAES DOS SANTOS <rafael.moraes@bgp.local>
2025-02-19 14:55:33 -0300

    v1.118

--- Backend/controllers/characterController.js
+++ Backend/controllers/characterController.js
@@ -9,11 +9,29 @@ async function getPlayersInSantuario() {
 async function regenerarSantuario() {
     try {
         const jogadoresNoSantuario = await getPlayersInSantuario();
+
+        console.log(`Regenerando santuário para ${jogadoresNoSantuario.length} jogadores.`);
+
     
         for (const jogador of jogadoresNoSantuario) {
             jogador.status.hp = Math.min(jogador.status.hp + 1, jogador.status.hpMax);
             jogador.status.mana = Math.min(jogador.status.mana + 1, jogador.status.manaMax);
             
+
+                        // Log para mostrar os detalhes do jogador e se houve alteração
+                        if (jogador.status.hp > hpAntigo || jogador.status.mana > manaAntiga) {
+                            console.log(`Jogador encontrado: ${jogador.name || jogador.ID}`);
+                            if (jogador.status.hp > hpAntigo) {
+                                console.log(`HP aumentou de ${hpAntigo} para ${jogador.status.hp}`);
+                            }
+                            if (jogador.status.mana > manaAntiga) {
+                                console.log(`Mana aumentou de ${manaAntiga} para ${jogador.status.mana}`);
+                            }
+                        } else {
+                            console.log(`Jogador ${jogador.name || jogador.ID} não teve aumento em HP ou Mana.`);
+                        }
+            
+                        
             await jogador.save(); // Salva as mudanças no banco
         }
     } catch (error) {
