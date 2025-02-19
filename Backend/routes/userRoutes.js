56b675f4788e735ab4406d0f8ba815e1beaa4cfc
RAFAEL VALQUER MORAES DOS SANTOS <rafael.moraes@bgp.local>
2025-02-19 14:19:29 -0300

    v1.118

--- Backend/routes/userRoutes.js
+++ Backend/routes/userRoutes.js
@@ -2,19 +2,12 @@ const express = require("express");
 const router = express.Router();
 const userController = require("../controllers/userController");
 
-// Rota para verificar usuário por telefone
-router.post("/checkUserByPhone", userController.checkUserByPhone);
 
-// Rota para verificar usuário por nome
-router.post("/checkUserByName", userController.checkUserByName);
 
-// Rota para verificar usuário por email
-router.post("/checkUserByEmail", userController.checkUserByEmail);
-
-// Rota para criar um novo usuário
-router.post("/createAccount", userController.createAccount);
-
-// Rota para atualizar o estado do usuário
+router.post("/check-number", userController.checkUserByPhone);
+router.post("/check-user", userController.checkUserByName);
+router.post("/check-email", userController.checkUserByEmail);
+router.post("/create-account", userController.createAccount);
 router.post("/updateUserState", userController.updateUserState);
 
 module.exports = router;
