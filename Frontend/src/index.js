0c7e8379d26a5bf0a5f68a431258b043eddc49e5
RAFAEL VALQUER MORAES DOS SANTOS <rafael.moraes@bgp.local>
2025-02-20 14:06:31 -0300

    v1.119

--- Frontend/src/index.js
+++ Frontend/src/index.js
@@ -369,7 +369,7 @@ Escolha uma missão para iniciar a sua jornada 🗺️:`
       );
 
       await client.sendMessage(
-        message.from, MessageMedia.fromFilePath(path.resolve(__dirname, 'assets/bandido.jpeg')), { caption: 'Bandido' });
+        message.from, MessageMedia.fromFilePath(path.resolve(__dirname, `assets/${enemy.enemyName}.jpeg`)));
       
 
       battleController[message.from].battle = new BattleSystem(
